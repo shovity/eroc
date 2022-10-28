36fdf95858585bafded3b1aebdde6beb3d6ba1cf
Shovity <shovity@gmail.com>
2022-10-28 02:11:21 +0000

    wrap gateway

--- modules/gateway.js
+++ modules/gateway.js
@@ -4,52 +4,56 @@ const httpProxy = require('http-proxy')
 const config = require('./config')
 
 
-const X_PROXY_KEY = 'x-proxy-forward-portal'
+module.exports = (option) => {
+    const router = epxress.Router()
+    const proxy = httpProxy.createProxyServer()
+    const key = 'x-eroc-proxy-forward'
 
-const router = epxress.Router()
-const proxy = httpProxy.createProxyServer()
+    proxy.on('proxyReq', (proxyReq, req, res, options) => {
+        proxyReq.setHeader(key, config.env)
 
-
-proxy.on('proxyReq', (proxyReq, req, res, options) => {
-    proxyReq.setHeader(X_PROXY_KEY, config.env)
-
-    if (req.body && req.headers['content-type'] && req.headers['content-type'].startsWith('application')) {
-        const bodyData = JSON.stringify(req.body)
-        
-        proxyReq.setHeader('Content-Type', 'application/json')
-        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData))
-        proxyReq.write(bodyData)
-    }
-})
-
-
-router.use('/:service', (req, res, next) => {
-    const service = req.params.service
-
-    // prevent circle forward
-    if (req.headers[X_PROXY_KEY] === config.env) {
-        return next()
-    }
-
-    // prevent owner forward
-    if (service === 'portal') {
-        return next()
-    }
-
-    // pass to local service
-    proxy.web(req, res, { target: `http://${service}:3000/api/${service}` }, (error) => {
-
-        if (config.env === 'loc') {
-            // pass to dev portal service
-            console.log(`gateway: proxy pass to develop server: ${req.originalUrl}`)
-            proxy.web(req, res, { target: `http://dev.portal.dichung.vn/api/${service}` }, (error) => {
-                return next(error)
-            })
-        } else {
-            return next(error)
+        if (req.body && req.headers['content-type'] && req.headers['content-type'].startsWith('application')) {
+            const bodyData = JSON.stringify(req.body)
+            
+            proxyReq.setHeader('Content-Type', 'application/json')
+            proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData))
+            proxyReq.write(bodyData)
         }
     })
-})
 
+    router.use('/:service', (req, res, next) => {
+        const service = req.params.service
 
-module.exports = router
+        // Prevent owner forward
+        if (service === config.service) {
+            return next()
+        }
+
+        // Prevent circle forward
+        if (req.headers[key] === config.env) {
+            return next()
+        }
+
+        // Check gateway whitelist service
+        if (config.gateway_whitelist && !config.gateway_whitelist.split(',').includes(service)) {
+            return next()
+        }
+
+        // Proxy pass to local service
+        proxy.web(req, res, { target: `http://${service}:3000/${service}` }, (error) => {
+
+            if (config.gateway_fallback) {
+
+                // Pass to developer center service
+                console.log(`gateway: fall back gateway ${config.gateway_fallback} - ${req.originalUrl}`)
+                proxy.web(req, res, { target: `${config.gateway_fallback}/${service}` }, (error) => {
+                    return next(error)
+                })
+            } else {
+                return next(error)
+            }
+        })
+    })
+
+    return router
+}
