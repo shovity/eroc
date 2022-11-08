81af5f747c75ba12d082b238d6dce49625c49937
Shovity <shovity@gmail.com>
2022-11-08 07:29:59 +0000

    add event

--- modules/event.js
+++ modules/event.js
@@ -1,3 +1,29 @@
-// const rediser = require('./rediser')
+const rediser = require('./rediser')
+const config = require('./config')
 
-// const
+const event = {}
+
+event.emit = (service, name, data) => {
+    const channel = `event:${service}:${name}`
+    rediser.pub(channel, data)
+}
+
+event.on = (name, handle) => {
+    check(handle.constructor.name === 'AsyncFunction', 'Param handle must be a AsyncFunction')
+
+    const channel = `event:${config.service}:${name}`
+
+    const wrap = (data) => {
+        handle(data)
+            .then(() => {
+                //
+            })
+            .catch((error) => {
+                console.error('Handle event failed', error)
+            })
+    }
+
+    rediser.sub(channel, wrap)
+}
+
+module.exports = event
