5cd8fad21e45987051cf0d84f0a7d10d92a52395
Shovity <shovity@gmail.com>
2022-11-14 06:46:57 +0000

    move create to cardinal

--- modules/cardinal.js
+++ modules/cardinal.js
@@ -1,19 +1,112 @@
+const http = require('http')
 const path = require('path')
 const fs = require('fs').promises
 const express = require('express')
-
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const exphbs = require('express-handlebars')
 const config = require('./config')
 const rediser = require('./rediser')
 const scanner = require('./scanner')
 const util = require('./util')
+const rio = require('./rio')
+const vanguard = require('./vanguard')
+const event = require('./event')
 
 const cardinal = {
     app: null,
+    server: null,
 }
 
-cardinal.boot = async (app) => {
-    cardinal.app = app
+cardinal.create = (middle) => {
+    const app = express()
+    const server = http.createServer(app)
 
+    cardinal.app = app
+    cardinal.server = server
+
+    cardinal.setup(middle).catch((error) => {
+        console.log('eroc: ERROR - Setup application failed:', error)
+    })
+
+    server.listen(config.port, () => {
+        console.log('eroc: 🍔 HTTP server ready!')
+    })
+
+    return { app, server }
+}
+
+cardinal.setup = async (middle) => {
+    const app = cardinal.app
+    const hbs = exphbs.create({ extname: 'html' })
+
+    app.engine('html', hbs.engine)
+    app.set('views', path.resolve(config.app_dir, config.seek_views))
+    app.set('view engine', 'html')
+
+    app.use(rio)
+    app.use(express.json({ limit: '10mb' }))
+    app.use(express.urlencoded({ extended: false }))
+    app.use(cookieParser())
+    app.use(cors())
+    app.use(vanguard.detect())
+
+    middle && middle(app)?.catch(console.error)
+
+    await cardinal.boot()
+    await cardinal.seek()
+    await cardinal.monitoring()
+
+    // Catch 404 route
+    app.use((req, res, next) => {
+        return res.error(
+            {
+                message: '404 Not found',
+                service: config.service,
+                url: req.originalUrl,
+                method: req.method,
+                env: config.env,
+            },
+            {
+                code: 404,
+            },
+        )
+    })
+
+    // Top level handle exception
+    app.use((error, req, res, next) => {
+        // Handle express error
+        // error throw from sync handle and next(err)
+
+        const response = {
+            message: error.message || error || 'server error',
+            service: config.service,
+            url: req.originalUrl,
+            method: req.method,
+            env: config.env,
+            level: 'fatal',
+        }
+
+        if (typeof error === 'object') {
+            Object.assign(response, error)
+        }
+
+        console.error(error)
+
+        return res.error(response)
+    })
+
+    if (config.api_monitor) {
+        const expressListEndpoints = require('express-list-endpoints')
+
+        console.log('eroc: 🧬 list apis')
+        expressListEndpoints(app).forEach((api) => {
+            api.methods.forEach((m) => console.log(`    ${m.padEnd(6)}${api.path}`))
+        })
+    }
+}
+
+cardinal.boot = async () => {
     // Load remote config from redis
     if (config.redis_uri) {
         check(config.service, 'Missing config.service')
@@ -122,7 +215,7 @@ cardinal.seek = async () => {
 }
 
 cardinal.monitoring = async () => {
-    rediser.sub(`service:${config.service}`, async (message) => {
+    event.on('cardinal', async (message) => {
         if (message.action === 'reboot') {
             await cardinal.shutdown()
             await cardinal.boot(cardinal.app)
