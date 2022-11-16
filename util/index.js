c0ca28765bb2cd77f552325cc291e2d40e9afe96
Shovity <shovity@gmail.com>
2022-11-16 10:19:10 +0000

    fix get util by proxy

--- util/index.js
+++ util/index.js
@@ -4,14 +4,14 @@ const util = {}
 
 const proxy = new Proxy(util, {
     get(target, prop) {
-        const name = `${prop}U`
+        const name = prop.slice(0, -1)
 
         // Load non-existent module to 'util'
         if (target[name] === undefined) {
-            target[name] = require(path.join(__dirname, prop))
+            target[name] = require(path.join(__dirname, name))
         }
 
-        return target[prop]
+        return target[name]
     },
 })
 
