5d87d01dd4627d7269aff06163ee2c3d13e76a87
shovity <shovity@gmail.com>
2023-05-24 15:00:49 +0700

    add telegram module

---
+++ modules/telegram.js
@@ -0,0 +1,24 @@
+const { request, config } = require('eroc')
+
+const telegram = {
+    base: 'https://api.telegram.org/bot'
+}
+
+telegram.send = async (id, text) => {
+    await config.deferred.config
+    check(config.telegram_token, 'Missing config.telegram_token')
+
+    return await request.post(`${telegram.base}${config.telegram_token}/sendMessage`, {
+        chat_id: id,
+        text,
+    })
+}
+
+telegram.method = async (method, param) => {
+    await config.deferred.config
+    check(config.telegram_token, 'Missing config.telegram_token')
+
+    return await request.post(`${telegram.base}${config.telegram_token}/${method}`, param)
+}
+
+module.exports = telegram
