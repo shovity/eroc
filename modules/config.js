1f00bad877d202d2102e5067097076b5b35b2ec5
shovity <shovity@gmail.com>
2023-12-08 02:19:18 +0000

    make color

--- modules/config.js
+++ modules/config.js
@@ -96,7 +96,7 @@ const main = async () => {
     config.deferred.config.resolve()
 
     process.nextTick(() => {
-        console.info(`config: 🍒 Load config done - service=${config.service}, env=${config.env}`)
+        console.info(`confg: 🍒 Load config done - service=${config.service}, env=${config.env}`)
     })
 }
 
