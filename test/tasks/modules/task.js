6d6eae65dabf4b1ea5acdcc7da1a9a10d173f4c6
shovity <shovity@gmail.com>
2024-03-29 04:26:27 +0000

    add test task trip

--- test/tasks/modules/task.js
+++ test/tasks/modules/task.js
@@ -9,9 +9,29 @@ task.on('hi', async (data) => {
     logger.info('logger path:tasks/modules/task:inner handle')
 })
 
+task.on('test1', async (data, meta) => {
+    if (meta.trips.lastIndexOf('test1') > 0) {
+        test.check('task trip', true)
+        return
+    }
+
+    task.emit('test2')
+})
+
+task.on('test2', async (data, meta) => {
+    task.emit('test3')
+})
+
+task.on('test3', async (data, meta) => {
+    task.emit('test1')
+})
+
 setTimeout(() => {
     test.start('task basic')
     task.emit('hi', 1)
+
+    test.start('task trip')
+    task.emit('test1')
 }, 3000)
 
 test.start('logger path:tasks/modules/task:outer handle')
