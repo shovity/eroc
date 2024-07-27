ee09844976b440bc4c717aaaa5f7453e9ded0fa7
shovity <shovity@gmail.com>
2024-07-27 14:14:45 +0000

    add declare

---
+++ util/index.d.ts
@@ -0,0 +1,9 @@
+import * as phone from './phone'
+import * as place from './place'
+import * as string from './string'
+import * as time from './time'
+
+export declare const phoneU: typeof phone
+export declare const placeU: typeof place
+export declare const stringU: typeof string
+export declare const timeU: typeof time
